d2235e8561d22c7c9de4c1b9d8e5a39fd50a8661
steliosk9 <steliosk9@gmail.com>
2019-07-23 21:03:33 -0500

    routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,21 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Cocktails.findAll({}).then(function(results) {
       res.render("index", {
         msg: "Welcome to our drink app!",
-        examples: dbExamples
+        examples: results
       });
     });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
+    db.findOne({ where: { id: req.params.id } }).then(function(results) {
       res.render("example", {
-        example: dbExample
+        example: results
       });
     });
   });
