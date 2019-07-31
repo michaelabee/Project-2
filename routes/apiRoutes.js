aba710063a1ff3a1f6ad70c8a4c71b2d3b39ea15
michaelabee <47824593+michaelabee@users.noreply.github.com>
2019-07-30 21:14:50 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,18 @@
 var db = require("../models");
-
+console.log("api routes loaded");
 module.exports = function(app) {
   // Get all drinks
   app.get("/api/cocktails", function(req, res) {
+    console.log("get api/cocktails");
     db.Cocktails.findAll({}).then(function(dbCocktails) {
+      console.log(dbCocktails);
       res.json(dbCocktails);
     });
   });
-
+​
   // Create a new drink
   app.post("/api/cocktails", function(req, res) {
+    console.log(" post /api/");
     console.log(req.body);
     db.Cocktails.create({
       name: req.body.name,
@@ -22,16 +25,47 @@ module.exports = function(app) {
       alcoholic: req.body.alcoholic,
       description: req.body.description
     }).then(function(dbCocktails) {
+​
       res.json(dbCocktails);
     });
   });
-
+​
+  // Create a new drink
+  app.put("/api/cocktails/favorite/", function(req, res) {
+    //logic to update database with id to favorite/unfavorite t or f
+    console.log(req);
+    db.Cocktails.update({
+      favorite: req.body.favorite
+  
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbCocktails) {
+      res.json(dbCocktails);
+    });
+​
+  });
+​
   // Delete an drink by id
   app.delete("/api/cocktails/:id", function(req, res) {
     db.Cocktails.destroy({ where: { id: req.params.id } }).then(function(
       dbCocktails
     ) {
       res.json(dbCocktails);
+    });
+  });
+​
+  app.get("/api/cocktails/:topic", function(req, res) {
+    console.log("app.get route search");
+    console.log(req.params.topic);
+    db.Cocktails.findAll({ where: { name: req.params.topic } }).then(function(
+      data
+    ) {
+      console.log("findone: ", data);
+      // create object
+      // res.render("page", object)
+      res.json(data);
     });
   });
 };
